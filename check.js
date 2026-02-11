d5f5bc487e871afa48a90f3be3360b3101b30eb3
flukhomkrit <125044207+flukhomkrit@users.noreply.github.com>
2026-02-11 15:32:11 +0700

    Add files via upload

--- check.js
+++ check.js
@@ -42,7 +42,7 @@ const FormData = require("form-data");
     console.log("Full size:", fullSize);
 
     const cropWidth = fullSize.width - 300;   // ตัดขวา
-    const cropHeight = fullSize.height - 100; // ตัดล่าง
+    const cropHeight = fullSize.height - 80; // ตัดล่าง
 
     await page.screenshot({
       path: "cropped.png",
